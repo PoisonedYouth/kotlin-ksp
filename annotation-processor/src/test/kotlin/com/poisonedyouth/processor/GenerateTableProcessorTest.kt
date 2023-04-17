74ebb9a75e69cd5d4bbf5c3d1ef0dd595e1048d8
Matthias Schenk <matthias.schenk@sevdesk.de>
2023-04-17 20:23:02 +0200

    [V-2-2] Added additional test

--- annotation-processor/src/test/kotlin/com/poisonedyouth/processor/GenerateTableProcessorTest.kt
+++ annotation-processor/src/test/kotlin/com/poisonedyouth/processor/GenerateTableProcessorTest.kt
@@ -136,6 +136,69 @@ class GenerateTableProcessorTest {
     }
 
     @Test
+    fun `should generate valid table object with unique index`() {
+        // given
+        val source = """
+                import com.poisonedyouth.annotation.GenerateTable
+                import com.poisonedyouth.annotation.PrimaryKey
+                import com.poisonedyouth.annotation.UniqueIndex
+                import java.time.LocalDate
+                
+                @GenerateTable(lowerCase = true)
+                data class User(
+                    @PrimaryKey
+                    val id: Long,
+                    @UniqueIndex(uniqueKey="name")
+                    val firstName: String,
+                    val lastName: String,
+                    val birthDate: LocalDate
+                )
+           """
+
+        // when
+        val compilation = KotlinCompilation().apply {
+            inheritClassPath = true
+            kspWithCompilation = true
+
+
+            sources = listOf(
+                SourceFile.kotlin("User.kt", source)
+            )
+            symbolProcessorProviders = listOf(
+                GenerateTableProcessorProvider()
+            )
+        }
+        val result = compilation.compile()
+        assertEquals(KotlinCompilation.ExitCode.OK, result.exitCode)
+
+        // then
+        val expectedResult = """
+                import java.time.LocalDate
+                import kotlin.String
+                import org.jetbrains.exposed.dao.id.LongIdTable
+                import org.jetbrains.exposed.sql.Column
+                import org.jetbrains.exposed.sql.javatime.date
+                
+                public object UserTable : LongIdTable("user","id") {
+                  public val firstName: Column<String> = varchar("firstName", 255).uniqueIndex("name")
+                
+                  public val lastName: Column<String> = varchar("lastName", 255)
+                
+                  public val birthDate: Column<LocalDate> = date("birthDate")
+                }
+           """
+
+        val generated = File(
+            compilation.kspSourcesDir,
+            "kotlin/UserTable.kt"
+        )
+        assertEquals(
+            expectedResult.trimIndent(),
+            generated.readText().trimIndent()
+        )
+    }
+
+    @Test
     fun `should generate valid table object with tablename`() {
         // given
         val source = """
